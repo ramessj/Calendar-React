84c6c1f7344722bc72699e9fdf33014d16bd22f5
Rama <ramiroquesadaperez@gmail.com>
2023-05-12 07:11:43 -0300

    prueba deploy

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,14 +1,11 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './routes';
 import { store } from './store';
 
 export const CalendarApp = () => {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
 				<AppRouter />
-			</BrowserRouter>
 		</Provider>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 import { CalendarApp } from './CalendarApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	
+	<HashRouter>
 		<CalendarApp />
-	
+	</HashRouter>
 );
